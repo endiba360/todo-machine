c2574c1677191335c957f040b07c90ad91faab19
endiba360 <enrique.diaz@alumnos.udg.mx>
2024-06-14 12:36:53 -0600

    feat: Iterate and visualize each task from array with TodoList component

--- src/App.js
+++ src/App.js
@@ -6,18 +6,25 @@ import { CreateTodoButton } from "./CreateTodoButton";
 import React from "react";
 import "./App.css";
 
+const taskList = [
+  { key: 1, text: "Put gas on the yellow car", completed: false },
+  { key: 2, text: "Buy eggs from the store", completed: false },
+  { key: 3, text: "Send email to University", completed: true },
+  { key: 4, text: "Feed the ducks", completed: true },
+];
+
 function App() {
   return (
     <React.Fragment>
-      <TodoCounter completed={5} total={10}/>
+      <TodoCounter completed={5} total={10} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {taskList.map((e) => (
+          <TodoItem key={e.key} text={e.text} completed={e.completed} />
+        ))}
       </TodoList>
+
       <CreateTodoButton />
     </React.Fragment>
   );
