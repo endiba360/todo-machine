59865d3cedd279e7e865dbb98109ad7ad6f7760f
endiba360 <enrique.diaz@alumnos.udg.mx>
2024-06-14 13:53:32 -0600

    feat: Change render tag by changing tag, eliminate default app styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import React from "react";
+import { CreateTodoButton } from "./CreateTodoButton";
 import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
-import { CreateTodoButton } from "./CreateTodoButton";
-import React from "react";
-import "./App.css";
 
 const taskList = [
   { key: 1, text: "Put gas on the yellow car", completed: false },
@@ -15,7 +14,7 @@ const taskList = [
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <TodoCounter completed={5} total={10} />
       <TodoSearch />
 
@@ -26,7 +25,7 @@ function App() {
       </TodoList>
 
       <CreateTodoButton />
-    </React.Fragment>
+    </>
   );
 }
 
