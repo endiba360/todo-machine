9eacf928b9352eed774f242056ce129e8f5bbba7
endiba360 <enrique.diaz@alumnos.udg.mx>
2024-06-14 18:31:10 -0600

    feat: Add dynamic styles to TodoItem component

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,9 +3,11 @@ import "./TodoItem.css";
 function TodoItem({ text, completed }) {
   return (
     <li className="TodoItem">
-      <span className="Icon Icon-check Icon-check--active">V {completed}</span>
-      <p className="TodoItem-p TodoItem-p--complete">{text}</p>
-      <span className="Icon Icon-delete">X</span>
+      <span className={`Icon Icon-check ${completed && "Icon-check--active"}`}>
+        V {completed}
+      </span>
+      <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>{text}</p>
+      <span className={`Icon Icon-delete`}>X</span>
     </li>
   );
 }
