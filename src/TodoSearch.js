f86a66a5867eca24c005a7c31f2d67a53e27e85c
endiba360 <enrique.diaz@alumnos.udg.mx>
2024-06-14 19:24:46 -0600

    feat: Add interaction to component with events

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,19 @@
 import "./TodoSearch.css";
 
 function TodoSearch() {
-  return <input className="TodoSearch" type="text" placeholder="Search for a Task..." />;
+  return (
+    <input
+      className="TodoSearch"
+      type="text"
+      placeholder="Search for a Task..."
+      onChange={(event) => {
+        console.log("You wrote in search component!");
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
