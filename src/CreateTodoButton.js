af60be3998c5fc2c0d58916cc03635d25560fa56
endiba360 <enrique.diaz@alumnos.udg.mx>
2024-06-14 19:23:54 -0600

    feat: Add interaction with events

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,7 +1,17 @@
 import "./CreateTodoButton.css";
 
 function CreateTodoButton() {
-  return <button className="CreateTodoButton">X</button>;
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={(event) => {
+        console.log(event);
+        console.log("You clicked on create component!");
+      }}
+    >
+      X
+    </button>
+  );
 }
 
 export { CreateTodoButton };
