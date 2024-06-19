73136f3b2d18bdaf60e38d96c1137c0556284942
endiba360 <enrique.diaz@alumnos.udg.mx>
2024-06-19 00:09:01 -0600

    feat: Add onComplete to component props and onClick event to change when condition result is recived

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,17 @@
 import "./TodoItem.css";
 
-function TodoItem({ text, completed }) {
+function TodoItem({ text, completed, onComplete }) {
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${completed && "Icon-check--active"}`}>
-        V {completed}
+      <span
+        className={`Icon Icon-check ${completed && "Icon-check--active"}`}
+        onClick={onComplete}
+      >
+        V
       </span>
-      <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>{text}</p>
+      <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>
+        {text}
+      </p>
       <span className={`Icon Icon-delete`}>X</span>
     </li>
   );
